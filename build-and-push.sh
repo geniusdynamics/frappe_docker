c0a823b640d9caa7866d0ce20af80a118fc1bab0
Kemboi Elvis <kemboielvis22@gmail.com>
2025-11-13 01:53:26 +0300

    few changes

--- build-and-push.sh
+++ build-and-push.sh
@@ -55,7 +55,6 @@ fetch_latest_release() {
   local api_url="https://api.github.com/repos/frappe/erpnext/releases"
   local headers=(
     -H "Accept: application/vnd.github+json"
-    -H "Authorization: Bearer $GH_TOKEN"
     -H "X-GitHub-Api-Version: 2022-11-28"
   )
 
